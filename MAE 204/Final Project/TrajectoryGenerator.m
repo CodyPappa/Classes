1bf411e863d06c0577e28a59395598470cd2aafc
Cody Pappa <codypappa@gmail.com>
2024-03-11 22:36:37 -0700

    finished milestone 1
    happy with the traj generator :)

--- MAE 204/Final Project/TrajectoryGenerator.m
+++ MAE 204/Final Project/TrajectoryGenerator.m
@@ -1,36 +1,58 @@
 function [outputConfigs] = TrajectoryGenerator(TseI,TscI, TscF, TceGrasp, TceStandoff, k)
-%TrajectoryGenerator
-%Summary of this function goes here
-%   Detailed explanation goes here
+% Trajectory Generator
+% Takes TseI: The initial config of the EE
+%      TscI: The initial config of the cube
+%      TscF: The final config of the cube
+%      TceGrasp: The config of the EE relative to the cube
+%      TceStandoff: The config of the EE relative to the cube to approach
+%      the cube from
+%      k: number of trajectory reference configs per 0.01 seconds. must be
+%      1 or greater
+% Returns outoutConfigs: an array containing all the configurations of the
+%      EE, one T per line, in order: r11,r12,r13,r21,r22,r23,r31,r32,r33, 
+%      px, py, pz, gripperstate
 
+% This function calculates a trajectory corresponding to the screw motion
+% from TseI through all 8 defined trajectories. Outputs a CSV file that can
+% be loaded into CoppeliaSim to simulate EE trajectory for the task
+if k<1
+    error('k not greater then 1')
+end
 
-%% create Tse positions for all 8 trajectoryies
+%% create Tse positions for all 8 trajectories
 Startpos = TseI;
-Traj{1,1} = TscI*TceStandoff;
-Traj{2,1}= TscI*TceGrasp;
-Traj{3,1}= Traj{2,1}; %same postion, just close the gripper and wait
-Traj{4,1}= Traj{1,1}; %back up to Pickup Standoff position, just with gripper closed
-Traj{5,1}= TscF*TceStandoff;
-Traj{6,1}= TscF*TceGrasp;
-Traj{7,1}= Traj{6,1}; %same position just open gripper and wait
-Traj{8,1}= Traj{5,1}; %back up to Final Standoff Position, with the gripper open
+pos{1} = TscI*TceStandoff;    %position of EE at block location, adjusted by standoff matrix
+pos{2}= TscI*TceGrasp;        % position of EE at block pickup location and oreintation 
+pos{3}= pos{2};                 %same postion, just close the gripper and wait
+pos{4}= pos{1};                 %back up to Pickup Standoff position, just with gripper closed
+pos{5}= TscF*TceStandoff;
+pos{6}= TscF*TceGrasp;
+pos{7}= pos{6};                 %same position just open gripper and wait
+pos{8}= pos{5};                 %back up to Final Standoff Position, with the gripper open
 %% add the state of the gripper for each trajectory
-Gripperstate = [0, 0, 0, 1, 1, 1, 1, 0, 0];
-
-
+Gripperstate = [0, 0, 1, 1, 1, 1, 0, 0, 0];
 
 %% simmulate Trajectorys between all 8 Tse positions
+%function is traj = ScrewTrajectory(Xstart, Xend, Tf, N, method)
+% set the time in seconds for each motion to occur
+t= [5 1 1 1 4 1 1 1];
+N= t.*k/.01; %how many intermeadate steps are needed for the motion to last T secconds
+traj(1,:)= ScrewTrajectory(Startpos, pos{1}, t(1), N(1),5);
+for n=2:8
+    traj(n,1:N(n))=ScrewTrajectory(pos{n-1}, pos{n}, t(n), N(n),5);
+end
 
 %% Output array to config_array.csv
 % config_array.csv will be located in Matlab's current directory
-outputConfigs(1,:) = [Startpos(1,1),Startpos(1,2),Startpos(1,3),Startpos(2,1),Startpos(2,2),Startpos(2,3),Startpos(3,1),...
-    Startpos(3,2),Startpos(3,3),Startpos(1,4),Startpos(2,4),Startpos(3,4),Gripperstate(1)];
-for n=1:8
-    outputConfigs(n+1,:) = [Traj{n,1}(1,1),Traj{n,1}(1,2),Traj{n,1}(1,3),Traj{n,1}(2,1),Traj{n,1}(2,2),Traj{n,1}(2,3),Traj{n,1}(3,1),...
-    Traj{n,1}(3,2),Traj{n,1}(3,3),Traj{n,1}(1,4),Traj{n,1}(2,4),Traj{n,1}(3,4),Gripperstate(n+1)];
-end
-    
-    %outputConfigs(3,:) = [Traj2(1,1),Traj2(1,2),Traj2(1,3),Traj2(2,1),Traj2(2,2),Traj2(2,3),Traj2(3,1),...
-   % Traj2(3,2),Traj2(3,3),Traj2(1,4),Traj2(2,4),Traj2(3,4),Gripperstate(3)];
+% preallocate size of our output matrix
+leng= sum(N);
+outputConfigs = zeros(leng,13);
+line = 1; %a counter to output each Transformation matrix as a line
+for n=1:8  %n cycles through the 8 traj
+    for i= 1:N(n) % i cycles through the T matrices inside each traj
+    outputConfigs(line,:) = [traj{n,i}(1,1),traj{n,i}(1,2),traj{n,i}(1,3),traj{n,i}(2,1),traj{n,i}(2,2),traj{n,i}(2,3),traj{n,i}(3,1), traj{n,i}(3,2),traj{n,i}(3,3),traj{n,i}(1,4),traj{n,i}(2,4),traj{n,i}(3,4),Gripperstate(n)];
+    line =line+1;
+    end
+end    
 writematrix(outputConfigs,'config_array.csv')
 end
