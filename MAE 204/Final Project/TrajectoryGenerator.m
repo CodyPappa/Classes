c172922eb3449d0db350420c28371a3308fdb564
Cody Pappa <codypappa@gmail.com>
2024-03-11 17:44:33 -0700

    added space for in between traj

--- MAE 204/Final Project/TrajectoryGenerator.m
+++ MAE 204/Final Project/TrajectoryGenerator.m
@@ -6,14 +6,14 @@ function [outputConfigs] = TrajectoryGenerator(TseI,TscI, TscF, TceGrasp, TceSta
 
 %% create Tse positions for all 8 trajectoryies
 Startpos = TseI;
-Traj{1} = TscI*TceStandoff;
-Traj{2}= TscI*TceGrasp;
-Traj{3}= Traj{2}; %same postion, just close the gripper and wait
-Traj{4}= Traj{1}; %back up to Pickup Standoff position, just with gripper closed
-Traj{5}= TscF*TceStandoff;
-Traj{6}= TscF*TceGrasp;
-Traj{7}= Traj{6}; %same position just open gripper and wait
-Traj{8}= Traj{5}; %back up to Final Standoff Position, with the gripper open
+Traj{1,1} = TscI*TceStandoff;
+Traj{2,1}= TscI*TceGrasp;
+Traj{3,1}= Traj{2,1}; %same postion, just close the gripper and wait
+Traj{4,1}= Traj{1,1}; %back up to Pickup Standoff position, just with gripper closed
+Traj{5,1}= TscF*TceStandoff;
+Traj{6,1}= TscF*TceGrasp;
+Traj{7,1}= Traj{6,1}; %same position just open gripper and wait
+Traj{8,1}= Traj{5,1}; %back up to Final Standoff Position, with the gripper open
 %% add the state of the gripper for each trajectory
 Gripperstate = [0, 0, 0, 1, 1, 1, 1, 0, 0];
 
@@ -26,8 +26,8 @@ Gripperstate = [0, 0, 0, 1, 1, 1, 1, 0, 0];
 outputConfigs(1,:) = [Startpos(1,1),Startpos(1,2),Startpos(1,3),Startpos(2,1),Startpos(2,2),Startpos(2,3),Startpos(3,1),...
     Startpos(3,2),Startpos(3,3),Startpos(1,4),Startpos(2,4),Startpos(3,4),Gripperstate(1)];
 for n=1:8
-    outputConfigs(n+1,:) = [Traj{n}(1,1),Traj{n}(1,2),Traj{n}(1,3),Traj{n}(2,1),Traj{n}(2,2),Traj{n}(2,3),Traj{n}(3,1),...
-    Traj{n}(3,2),Traj{n}(3,3),Traj{n}(1,4),Traj{n}(2,4),Traj{n}(3,4),Gripperstate(n+1)];
+    outputConfigs(n+1,:) = [Traj{n,1}(1,1),Traj{n,1}(1,2),Traj{n,1}(1,3),Traj{n,1}(2,1),Traj{n,1}(2,2),Traj{n,1}(2,3),Traj{n,1}(3,1),...
+    Traj{n,1}(3,2),Traj{n,1}(3,3),Traj{n,1}(1,4),Traj{n,1}(2,4),Traj{n,1}(3,4),Gripperstate(n+1)];
 end
     
     %outputConfigs(3,:) = [Traj2(1,1),Traj2(1,2),Traj2(1,3),Traj2(2,1),Traj2(2,2),Traj2(2,3),Traj2(3,1),...
